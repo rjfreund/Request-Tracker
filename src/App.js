74ed7dee23142efa439ee41256a1e691a93d0ce1
R.J. Freund <robert.joseph.freund@gmail.com>
2022-05-23 16:10:55 -0500

    Added simple grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap'
 
 function App() {
   return (
@@ -17,7 +18,56 @@ function App() {
         >
           Learn React
         </a>
+
+
+
       </header>
+      <div role="region" aria-label="Code Example" className="show-grid">
+      <Container fluid>
+        <Row>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="/static/media/logo.6ce24c58023cc2f8fd88fe9d219db6c6.svg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={8}>
+            xs=12 md=8
+          </Col>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+        </Row>
+
+        {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
+        <Row>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+        </Row>
+
+        {/* Columns are always 50% wide, on mobile and desktop */}
+        <Row>
+          <Col xs={6}>xs=6</Col>
+          <Col xs={6}>xs=6</Col>
+        </Row>
+      </Container>
+      </div>
     </div>
   );
 }

d38ea4f439253129143277c04e36fc6d83c5c55e
R.J. Freund <robert.joseph.freund@gmail.com>
2022-05-30 12:59:14 -0500

    -

--- src/App.js
+++ src/App.js
@@ -1,72 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App pt-4">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <img src="https://www.google.com/logos/doodles/2022/memorial-day-2022-united-states-6753651837109423-l.png" className="card-img-top" alt="blah" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <img src="https://www.google.com/logos/doodles/2022/memorial-day-2022-united-states-6753651837109423-l.png" className="card-img-top" alt="blah" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <img src="https://www.google.com/logos/doodles/2022/memorial-day-2022-united-states-6753651837109423-l.png" className="card-img-top" alt="blah" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+        </div>
 
-
-
-      </header>
-      <div role="region" aria-label="Code Example" className="show-grid">
-      <Container fluid>
-        <Row>
-          <Col>
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="/static/media/logo.6ce24c58023cc2f8fd88fe9d219db6c6.svg" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} md={8}>
-            xs=12 md=8
-          </Col>
-          <Col xs={6} md={4}>
-            xs=6 md=4
-          </Col>
-        </Row>
-
-        {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
-        <Row>
-          <Col xs={6} md={4}>
-            xs=6 md=4
-          </Col>
-          <Col xs={6} md={4}>
-            xs=6 md=4
-          </Col>
-          <Col xs={6} md={4}>
-            xs=6 md=4
-          </Col>
-        </Row>
-
-        {/* Columns are always 50% wide, on mobile and desktop */}
-        <Row>
-          <Col xs={6}>xs=6</Col>
-          <Col xs={6}>xs=6</Col>
-        </Row>
-      </Container>
       </div>
     </div>
   );
